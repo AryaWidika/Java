a18ef9103313412e03f6e687ece228c1b2dce7cb
AryaWidika <164707616+AryaWidika@users.noreply.github.com>
2024-03-24 09:49:34 +0700

    Add files via upload

---
+++ latihanjava1/LatihanJava1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanjava1;
+
+/**
+ *
+ * @author Arya Widika
+ */
+public class LatihanJava1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ latihanjava1/OptAritmatika.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanjava1;
+
+/**
+ *
+ * @author Asus
+ */
+public class OptAritmatika {
+    
+}
